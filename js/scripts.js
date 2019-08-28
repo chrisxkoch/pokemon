6c6d939c40ccffb6b5c80a100ccd18f6c424398d
chrisxkoch <chrxkoch@icloud.com>
2019-08-28 13:40:24 +0200

    Update scripts.js
    Changed JS-File. Including correct Array with different objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,8 @@
 
-var pokemonweight = {
-  Bulbasaur: 15,
-  Charmander: 18,
-  Squirtle: 19,
-};
-var pokemonability = {
-  Bulbasaur: ['overgrow'],
-  Charmander: ['blaze'],
-  Squirtle: ['torrent'],
-};
 
+var information = [
+  {name: 'Bulbasaur' , ability: 'overgrow' , weight: '15', type: ['grass, poison']},
+  {name: 'Charmander', ability: 'blaze', weight: '18', type: ['fire']},
+  {name: 'Squirtle', ability: 'torrent', weight: '19', type: ['water']},
+];
 
-var pokemon = {
-  Bulbasaur:  {
-      Weight: 15,
-      Type: ['grass', 'poison']}
-};
-// I wanted to include all information in one, but failed...
-
-document.querySelector('button').addEventListener('click', () => {
-  
-  var currentUserName = document.querySelector('#pokemon').value;
-  
-  document.querySelector('#pokemon_key').innerText = currentUserName;
-  document.querySelector('#weight').innerText = pokemonweight[currentUserName];
-  document.querySelector('#abilities').innerText = pokemonability[currentUserName];
-  document.querySelector('#pokemon').innerText = pokemon[currentUserName];
-  
-});
