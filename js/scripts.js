f5e47e65ed48a9c035dc6950a45c0aed7bcfeced
chrisxkoch <chrxkoch@icloud.com>
2019-09-13 14:49:11 +0200

    finally

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-
-
 var information = [
-  {name: 'Bulbasaur' , ability: 'overgrow' , weight: '15', type: ['grass, poison']},
-  {name: 'Charmander', ability: 'blaze', weight: '18', type: ['fire']},
-  {name: 'Squirtle', ability: 'torrent', weight: '19', type: ['water']},
+  {
+    name: 'Bulbasaur', 
+    height: 0.7, 
+    types: ['grass, poison']
+  },
+  {
+    name: 'Charmander',
+    height: 1.7,
+    types: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.9, 
+    types: ['water']
+  }
 ];
 
+for (var i = 0; i < information.length; i++) {
+  if (information[i].height < 1) {
+  document.write("<button>" + information[i].name + "</button>" + ' ' + information[i].height + "<br >");
+} else {
+  document.write("<button>" + information[i].name + "</button>" + ' ' + information[i].height + ' ' + 'Wow, that is big!' + "<br >");
+}
+}
+
+ 
