c960d9301a6e69814fb61aff38fbc7583232f0c0
chrisxkoch <chrxkoch@icloud.com>
2019-10-14 18:08:52 +0200

    finally. formatted and corrected.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,40 @@
-var pokemonRepository = (function () {
-  var repository = [
-  {
-    name: 'Bulbasaur', 
-    height: 0.7, 
-    types: ['grass, poison']
-  },
-  {
-    name: 'Charmander',
-    height: 1.7,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.9, 
-    types: ['water']
-  }];
+var pokemonRepository = (function() {
+  var repository = [{
+          name: 'Bulbasaur',
+          height: 0.7,
+          types: ['grass, poison']
+      },
+      {
+          name: 'Charmander',
+          height: 1.7,
+          types: ['fire']
+      },
+      {
+          name: 'Squirtle',
+          height: 0.9,
+          types: ['water']
+      }
+  ];
 
-  function add (pokemon) {
-repository.push(pokemon);
+  function add(pokemon) {
+      repository.push(pokemon);
   }
+
   function getAll() {
-    return repository;
+      return repository;
   }
-return {
-  add: add,
-  getAll: getAll
-};
+  return {
+      add: add,
+      getAll: getAll
+  };
 })();
 
-  
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.add(pokemon);
- 
-  });
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.add(pokemon);
+
+  document.write(pokemon.name + " ")
+  document.write(pokemon.height + " ")
+  document.write(pokemon.types + " " + "<br>")
+
+
+});
